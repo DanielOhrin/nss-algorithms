4a59458cd9aecd0791e86e896407c5a51fe6a49a
Daniel Ohrin <xqiam@outlook.com>
2022-09-16 09:46:52 -0500

    Created the Algorithm

--- main.js
+++ main.js
@@ -6,5 +6,24 @@
     // Return only a boolean
 
 const isPalindrome = (string) => {
-    const slicePoint
+    let wordArr = [...string]
+
+    let secondHalf
+
+    if (wordArr.length % 2 === 0) {
+        secondHalf = wordArr.slice(Math.floor(wordArr.length / 2))
+        wordArr.splice(Math.floor(wordArr.length / 2))
+    } else {
+        secondHalf = wordArr.slice(Math.floor(wordArr.length / 2) + 1)
+        wordArr.splice(Math.floor(wordArr.length / 2))
+    }
+
+    return wordArr.join("") === secondHalf.reverse().join("") ? true : false
 }
+
+console.log(isPalindrome('awoieh')) // False
+console.log(isPalindrome('tattarrattat')) // True
+console.log(isPalindrome('racecar')) // True
+console.log(isPalindrome('marshmallow')) // False
+console.log(isPalindrome('rotator')) // True
+
