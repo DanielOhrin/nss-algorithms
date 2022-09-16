0e534a342336d4977fc175e41a6114a24a09fd1e
Daniel Ohrin <xqiam@outlook.com>
2022-09-16 09:37:13 -0500

    Initial Commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// Define a function that checks if a string is a palindrome.
+    // Find the midway point
+    // Slice
+    // Splice
+    // Compare the two halves
+    // Return only a boolean
+
+const isPalindrome = (string) => {
+    const slicePoint
+}
