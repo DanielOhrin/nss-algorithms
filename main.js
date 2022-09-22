a1d8986c1b5952fb42262dabce04b386c65431cd
Daniel Ohrin <xqiam@outlook.com>
2022-09-22 15:53:48 -0500

    Initial Commit

--- main.js
+++ main.js
@@ -1,26 +1,3 @@
-# Product Checksums
-
-Late at night, you sneak into the warehouse and use your fancy wrist device to quickly scan every box and produce a list of the likely candidates.
-
-To make sure you didn't miss any, you scan the likely candidate boxes again, counting the number that have an ID containing exactly two of any letter and then separately counting those with exactly three of any letter. You can multiply those two counts together to get a rudimentary checksum and compare it to what your device predicts.
-
-For example, if you see the following box IDs:
-
-* abcdef contains no letters that appear exactly two or three times.
-* bababc contains two a and three b, so it counts for both.
-* abbcde contains two b, but no letter appears exactly three times.
-* abcccd contains three c, but no letter appears exactly two times.
-* aabcdd contains two a and two d, but it only counts once.
-* abcdee contains two e.
-* ababab contains three a and three b, but it only counts once.
-
-Of these box IDs, four of them contain a letter which appears exactly twice, and three of them contain a letter which appears exactly three times. Multiplying these together produces a checksum of 4 * 3 = 12.
-
-What is the checksum for your list of box IDs?
-
-## Product IDs
-
-```js
 const productIds = ["ubkfmdjxyzlbgkrotcepvswaqx", "uikfmdkuyzlbgerotcepvswaqh", "uikfmdpxyzlbgnrotcepvswoeh", "ikfmdjxyzlbgnrotqepvswyqh", "qgkfmdjxyzlbgnmotcepvswaqh", "uikfmdjxyzqbgnrytcepvsbaqh", "uikfmdjxyzibgprotcecvswaqh", "uikfmajxyzlcgnrojcepvswaqh", "uvkfsdjxyzlbgnrotcepvjwaqh", "uikfmdjxfzlbggrotcepvswawh", "uikfmhjxyzlbgnuotcepvjwaqh", "uikfmdjxyzlbuzcotcepvswaqh", "uikfmdwxyzlbgnrotcepvfwamh", "uikfmdexyzlbgnroecepvswhqh", "uikfmdjuyzlbgnrotcqpvswafh", "uikfddjxyzvbgnrotceppswaqh", "yikfwdjxyzlbgnrotcepvswagh", "uiktmdjxyzlbgnrotceposwajh", "uikfmdsxyzlbgnroteetvswaqh", "uikfpdjxyzlbgnroncepvswuqh", "uikfmtjxyzlbgurotcepvswaoh", "eikfmdjxyjlbgnrotcepyswaqh", "uikfkdjxyzlbgnrotcepvszaqv", "uikfrdjxtwlbgnrotcepvswaqh", "uikfmdjxyzlbgnrotpepwswahh", "kikfmdjxyzlbgnkotcepvswqqh", "uikfkduxyzlbgnrotcepvswafh", "uikfxhjxyzlbgnrotcegvswaqh", "uikfmdjxyzlmgnrotcenvawaqh", "uzkfmddxyzlbgnrltcepvswaqh",
 "uikfmdjxyzlbgnrobcepisqaqh", "uijfmdjxyzlbgnrotcexvrwaqh", "uiwjmdjxyzlbgnrotceprswaqh", "uhkqmdjxwzlbgnrotcepvswaqh", "uiktmsjxyzwbgnrotcepvswaqh", "uikfmdjxyztbgnqotcepvswcqh", "uibfmdjxyzlbgnroqcepvswaqx", "uwkfmdjxyxlbgnrotcfpvswaqh", "uikumdjxyzlbgnrstceposwaqh", "uikfzdjxyznhgnrotcepvswaqh", "uikuydjxyzlbgnrotqepvswaqh", "uikfmdqxyzlbgnrotfefvswaqh", "yikfmdjxyzlbrnrqtcepvswaqh", "uiifmdjxyzlbenrotcfpvswaqh", "uxkjmdjxyzlbgnrotcmpvswaqh", "uikgmdjxyzlbgnrotceovlwaqh", "uikfmdjxyzvbgzrotcenvswaqh", "uiufmdjxyzlbgnrotceposwazh", "uiafmdjxyzlignmotcepvswaqh", "uikfmdjxyzwbgnsotlepvswaqh", "uikjmdjvyzlbgnrotcenvswaqh", "uikfmdjxyzlbonroteepvswaqi", "uikfmdjxyzldgnroxcepviwaqh", "uikvmdjxyzlbgdrotrepvswaqh", "uikfmdjyyzwbgnrotcepvzwaqh", "uikfmdjxyzzbnnvotcepvswaqh", "uikomdjxyzlbgnrotcepvuwhqh", "uikfmmjxyzbbgnrotcepvswayh", "uikfmdjfezlbgprotcepvswaqh", "uzkfmojxmzlbgnrotcepvswaqh",
 "uipfmdjxyzlbgnrotceyuswaqh", "uikfmdjxyzlkgnmotcepvswadh", "uikfmdjxyzlbgnuctcbpvswaqh", "uikfqdjxyzlbogrotcepvswaqh", "uikfmdjxyzlfynrotcepvswash", "uikfmdjxizzmgnrotcepvswaqh", "uhkfmdjxyzlbhnrotcenvswaqh", "uipfmdjxyzlbgorotcepfswaqh", "uikfmdjxyznbgnrotcepvswfah", "uikfmujxyzlbgnrotnepvssaqh", "uikfmdjxyzlxgnrotcepvsrwqh", "uikfmdjxszlbgnrottvpvswaqh", "umkfmdskyzlbgnrotcepvswaqh", "uikfmdjxyzlbgorotcwpzswaqh", "uikfmdhxyzzbgnzotcepvswaqh", "jikfmdjxyzlbgnrotcepveyaqh", "uirfmdlxyzlbgnjotcepvswaqh", "uikfmdjxyzlbgnrorxepvswazh", "uikfmdjxyzltgnrotcepvsxaqi", "uikfmdjxyzlbxlrotcepkswaqh", "uvkfmdjxyzlbgnrotcopvswxqh", "uxkfmdjxkzlbgnrotcepvswqqh", "uikfmdjxyzlbqnrotcepvhwrqh", "uikfmdjxyzlvgnrolcepvswrqh", "urkfmdixyzlbgnrotcepvsjaqh", "uikfmdjxymlbsnrotcepvswcqh", "uikfmdjxyilbgnrotcepvcwzqh", "uikfadjxlzlbgnrotcepvswaql", "uikfmdjxuzlbgdrotcgpvswaqh", "yikfmdgxyzlbgnrotcepvswarh",
@@ -30,4 +7,43 @@ const productIds = ["ubkfmdjxyzlbgkrotcepvswaqx", "uikfmdkuyzlbgerotcepvswaqh", 
 "uikfpdyxyzlbgnrotcepvswagh", "uikfmdjxyzobgnrotrlpvswaqh", "zisdmdjxyzlbgnrotcepvswaqh", "uikfmdjxyzlbgnlotiesvswaqh", "uikfddixyzlbgnroucepvswaqh", "uijfmdrxyzlbgnrotoepvswaqh", "uikfmdcxbzlbgnrotcepvpwaqh", "uikfmdjxxzlbfnrotcecvswaqh", "upkfmdjxyzmtgnrotcepvswaqh", "uikfmdrxyzlbgnrjtcepvswaqp", "uizfmdjxyzlbsnrotcepviwaqh", "uidfmdjxyslbgnrotcxpvswaqh", "uikfmdjxyzlbgnrovyepvsdaqh", "uiafmdjxyzlbgnrxtcepvsdaqh", "ugkfmdjxyzlbgnrodcepvswawh", "pikfmtjxyzhbgnrotcepvswaqh", "uikfmdjxyzlfgnvotcepvswtqh", "uikbmdjxyzlbgerotcepvswaqm", "uikfmdjxyhlbdnrotcepvswaqy", "uikfgdjxyzlbgnhotcepvswdqh", "uikfmdpxyzlbgnrotcepvscanh", "uikfmdjxyzsbgnretcepvswaqn", "uikfddjxyzlrgnrotcepvsbaqh", "uikfmdjxyzlbgnrotcqnrswaqh", "uhkfmejxyzlbgnrotvepvswaqh", "uikimdjxyzlngnrotceprswaqh", "uikfmdjxyzwbunrotiepvswaqh", "rikfmdjxyzlbgnrotcypvssaqh", "uikfmdjxyzlbdnrotcrpvswsqh", "uekfmdjxkzlbznrotcepvswaqh",
 "uikfmdjxyglbgvrotcepvswaqv", "uikfmcjxyzlbgnrotmeovswaqh", "uikfmdjxyznbgnrozcepvswaqm", "uikfmdjxyzlbdnrotcepdsyaqh", "umkfmdjxfzlbgnrotiepvswaqh", "uitfmdjxyzvbcnrotcepvswaqh", "uikfmdjqyzlbgnrvtcepvlwaqh", "uikfmdjxyzkbworotcepvswaqh", "uikfmdzxyzlbwnrotcypvswaqh", "uikfmdjxyklbgnrftyepvswaqh", "uinfmsjxyzlbgnrotcepsswaqh", "xisfmdjxymlbgnrotcepvswaqh", "uikfmdjxjzlbgnropcepvswaqv", "uikfmdjxyalegyrotcepvswaqh", "uikfydjxyzlbgnrotcekvswtqh", "uikwmojxyzlbgnromcepvswaqh", "uikdmdjayzlbgnrotcepvswzqh", "uikfmdjxyzlmvnrotctpvswaqh", "uikfmbjxyzlbgnrotceptsweqh", "yvkfmdjxyzlbgqrotcepvswaqh", "uikomdjxfxlbgnrotcepvswaqh", "uikfmdjxczlbgnroocepgswaqh", "uikemdjxizlbgnrotcegvswaqh", "uikdmdjxyzlbgwrotceprswaqh", "tiyfmdjfyzlbgnrotcepvswaqh", "tmkfmdjxyzlbgirotcepvswaqh", "uikfmdjxyzebgnzotcepqswaqh", "uikfmljxyzlbgnrwtcepvswaeh", "uikfmojxyzlbgnrotcepbswwqh", "uikfmdjxyzlbgsrotcewvswwqh",
 "uikfmdjhyzlbgdrogcepvswaqh", "uikfmvjxyzlbrnrltcepvswaqh", "jikfmdjxyzlbgnrotcepvgcaqh", "uikhtdjxyzlbgnrotcepvswarh", "uikfmdjxyezbgnritcepvswaqh", "uikfwdjxyzlbgnrotzepvsnaqh", "uikfmdjxqylbgnrobcepvswaqh", "zikzmdjxyzlbgnrhtcepvswaqh", "uiksmzjxyzlbgnrftcepvswaqh", "uikfmdjxuzlbgnrotcepvsvaqc"]
-```
+
+// Define a function named CheckSums (array)
+    // Count how many strings have EXACTLY 2 of the same character
+    // Now count how many have EXACTLY 3 of the same character
+    // Multiply them together and return the sum
+
+const checkSums = (array) => {
+    let amountWithTwo = 0
+    let amountWithThree = 0
+
+    array.forEach(str => {
+        // Object to hold the values(amount) of each letter, and index for determining when to run the last if statements
+        let index = 0
+        let letterObj = {}
+
+        for (const letter of str) {
+            if (Object.keys(letterObj).includes(letter)) {
+                letterObj[letter] ++
+            } else {
+                letterObj[letter] = 1
+            }
+
+            if (index === str.length - 1) {
+                // Check the values of each letter
+                if (Object.values(letterObj).includes(2)) {
+                    amountWithTwo ++
+                }
+                if (Object.values(letterObj).includes(3)) {
+                    amountWithThree ++
+                }
+            }
+
+            index ++
+        }
+    })
+
+    return amountWithTwo * amountWithThree
+}
+
+console.log(checkSums(productIds))
